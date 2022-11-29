95dfe95a3fc1f86bf1ee347008718430cb593916
Olga <voron7zova.o@yandex.ru>
2022-11-29 22:46:12 +0300

    Программа

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿string[] arrayFirst = {"labas", "hi", "pryvitannie", "privet", "Ey"};
+int countFirst = CountStringLessThreeChar(arrayFirst);
+string[] resultFirst = FillResultArray(arrayFirst,countFirst);
+
+
+string[] arraySecond = {"333", "16", "-41", "my_sun", "abcd"};
+int countSecond = CountStringLessThreeChar(arraySecond);
+string[] resultSecond = FillResultArray(arraySecond,countSecond);
+
+
+string[] arrayThird = {"Poland", "Lithuania", "Belarus", "OAE"};
+int countThird = CountStringLessThreeChar(arrayThird);
+string[] resultThird = FillResultArray(arrayThird, countThird);
+
+void PrintArray(string[] array)
+{
+
+for (int i = 0; i < array.Length; i++)
+{
+if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+else Console.Write($"{array[i]}.");
+}
+Console.WriteLine();
+}
+
+int CountStringLessThreeChar(string[] array)
+{
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i].Length <= 3) count++;
+}
+return count;
+}
+
+string[] FillResultArray(string[] array, int count)
+{
+string[] result = new string[count];
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i].Length <= 3)
+{
+result[result.Length - count] = array[i];
+count--;
+}
+}
+return result;
+}
+
+PrintArray(resultFirst);
+PrintArray(resultSecond);
+PrintArray(resultThird);
+
